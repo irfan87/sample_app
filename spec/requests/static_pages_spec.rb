bd81a7bd6bbad9a0034a0f1d971bf283cb71dd8f
irfan87 <nerve2009@yahoo.com>
2016-03-01 04:46:23 +0800

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
+    end
+  end
+end
