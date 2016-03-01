25eb88c3de67b533fc56aa61d98a730686b49568
irfan87 <nerve2009@yahoo.com>
2016-03-01 20:08:55 +0800

    testing links, title and helper

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,13 +1,1 @@
-RSpec.configure do |config|
-  def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
-
-    if base_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
-  end
-
-  config.include ApplicationHelper
-end
+include ApplicationHelper
